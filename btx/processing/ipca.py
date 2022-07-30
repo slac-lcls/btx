a06229cdf4f9bf765aae67b0c5d3b2fab5c320d8
calhep <callumahepworth@gmail.com>
2022-07-29 17:13:18 -0700

    Included new modules.

---
+++ btx/processing/ipca.py
@@ -0,0 +1,285 @@
+import numpy as np
+from mpi4py import MPI
+
+from time import perf_counter
+
+class TaskTimer:
+    """
+    A context manager to record the duration of managed tasks.
+
+    Attributes
+    ----------
+    start_time : float
+        reference time for start time of task
+    intervals : list
+        list containing time interval data
+    """
+    
+    def __init__(self, intervals):
+        """
+        Construct all necessary attributes for the TaskTimer context manager.
+
+        Parameters
+        ----------
+        intervals : list of float
+            List containing interval data
+        """
+        self.start_time = 0.
+        self.intervals = intervals
+    
+    def __enter__(self):
+        """
+        Set reference start time.
+        """
+        self.start_time = perf_counter()
+    
+    def __exit__(self, *args, **kwargs):
+        """
+        Mutate interval list with interval duration of current task.
+        """
+        self.intervals.append(perf_counter() - self.start_time)
+
+class IPCA:
+
+    def __init__(self, d, q):
+
+        self.comm = MPI.COMM_WORLD
+        self.rank = self.comm.Get_rank()
+        self.size = self.comm.Get_size()
+
+        self.d = d
+        self.q = q
+
+        self.S = np.eye(self.q)
+        self.U = np.zeros((self.d, self.q))
+        self.mu = np.zeros((self.d, 1))
+        self.total_variance = np.zeros((self.d, 1))
+
+        self.get_distributed_indices()
+
+        # attributes for computing timings of iPCA steps
+        self.ipca_intervals['concat'] = []
+        self.ipca_intervals['ortho'] = []
+        self.ipca_intervals['build_r'] = []
+        self.ipca_intervals['svd'] = []
+        self.ipca_intervals['update_basis'] = []
+
+
+    def update_model(self, X):
+        d, m = X.shape
+        n = self.n
+
+        mu_m, s_m = calculate_sample_mean_and_variance(X)
+
+        X_pm_loc = None
+
+        with TaskTimer(self.ipca_intervals['concat']):
+            X_centered = X - np.tile(mu_m, m)
+            X_m = np.hstack((X_centered, np.sqrt(n * m / (n + m)) * (mu_m - self.mu)))
+
+        with TaskTimer(self.ipca_intervals['ortho']):
+            UX_m = self.U.T @ X_m
+            dX_m = X_m - self.U @ UX_m
+            X_pm, _ = np.linalg.qr(dX_m, mode='reduced')
+        
+        with TaskTimer(self.ipca_intervals['build_r']):
+            R = np.block([[self.S, UX_m], [np.zeros((m + 1, q)), X_pm.T @ dX_m]])
+        
+        with TaskTimer(self.ipca_intervals['svd']):
+            U_tilde, S_tilde, _ = np.linalg.svd(R)
+        
+        print(self.rank)
+
+        self.comm.Barrier()
+
+        with TaskTimer(self.ipca_intervals['update_basis']):
+            U_split = self.U[self.start_index:self.end_index, :]
+            X_pm_split = X_pm[self.start_index:self.end_index, :]
+
+            U_prime_partial = np.hstack((U_split, X_pm_split)) @ U_tilde
+            print(U_prime_partial.shape)
+            
+            U_prime = np.empty((d, q+m+1))
+
+            self.comm.Allgather(U_prime_partial, U_prime)
+
+            print(U_prime.shape)
+        
+        self.U = U_prime[:, :q]
+        self.S = np.diag(S_tilde[:q])
+
+        self.total_variance = update_sample_variance(self.total_variance, s_m, self.mu, mu_m, n, m)
+        self.mu = update_sample_mean(self.mu, mu_m, n, m)
+
+    def initialize_model(self, X):
+        self.mu, self.total_variance = calculate_sample_mean_and_variance(event_data)
+
+        centered_data = event_data - np.tile(self.mu, q)
+        self.U, s, _ = np.linalg.svd(centered_data, full_matrices=False)
+        self.S = np.diag(s)
+
+    def get_distributed_indices(self):
+        """
+        Distribute indices over ranks in MPI world.
+        """
+        split_indices = np.zeros(self.size)
+        for r in range(self.size):
+            num_per_rank = self.d // self.size
+            if r < (self.d % self.size):
+                num_per_rank += 1
+            split_indices[r] = num_per_rank
+        split_indices = np.append(np.array([0]), np.cumsum(split_indices)).astype(int)
+
+        self.start_index = split_indices[self.rank]
+        self.end_index = split_indices[self.rank + 1]
+
+    def report_interval_data(self):
+        """
+        Report time interval data gathered during iPCA.
+        """
+        print(self.rank)
+        if len(self.ipca_intervals) == 0:
+            print('iPCA has not yet been performed.')
+            return
+        
+        for key in list(self.ipca_intervals.keys()):
+            interval_mean = np.mean(self.ipca_intervals[key])
+
+            if key is 'load_event':
+                print(f'Mean time to load each observation: {interval_mean:.4g}s')
+            else:
+                print(f'Mean per-iteration compute time of step \'{key}\': {interval_mean / self.block_size:.4g}s')
+
+
+def calculate_sample_mean_and_variance(imgs):
+    """
+    Compute the sample mean and variance of a flattened stack of n images.
+
+    Parameters
+    ----------
+    imgs : ndarray, shape (d x n)
+        horizonally stacked batch of flattened images 
+
+    Returns
+    -------
+    mu_m : ndarray, shape (d x 1)
+        mean of imgs
+    su_m : ndarray, shape (d x 1)
+        sample variance of imgs (1 dof)
+    """
+    d, m = imgs.shape
+
+    mu_m = np.reshape(np.mean(imgs, axis=1), (d, 1))
+    s_m  = np.zeros((d, 1))
+
+    if m > 1:
+        s_m = np.reshape(np.var(imgs, axis=1, ddof=1), (d, 1))
+    
+    return mu_m, s_m
+
+def update_sample_mean(mu_n, mu_m, n, m):
+    """
+    Combine combined mean of two blocks of data.
+
+    Parameters
+    ----------
+    mu_n : ndarray, shape (d x 1)
+        mean of first block of data
+    mu_m : ndarray, shape (d x 1)
+        mean of second block of data
+    n : int
+        number of observations in first block of data
+    m : int
+        number of observations in second block of data
+
+    Returns
+    -------
+    mu_nm : ndarray, shape (d x 1)
+        combined mean of both blocks of input data
+    """
+    mu_nm = mu_m
+
+    if n != 0:
+        mu_nm = (1 / (n + m)) * (n * mu_n + m * mu_m)
+
+    return mu_nm
+
+def update_sample_variance(s_n, s_m, mu_n, mu_m, n, m):
+    """
+    Compute combined sample variance of two blocks of data described by input parameters.
+
+    Parameters
+    ----------
+    s_n : ndarray, shape (d x 1)
+        sample variance of first block of data
+    s_m : ndarray, shape (d x 1)
+        sample variance of second block of data
+    mu_n : ndarray, shape (d x 1)
+        mean of first block of data
+    mu_m : ndarray, shape (d x 1)
+        mean of second block of data
+    n : int
+        number of observations in first block of data
+    m : int
+        number of observations in second block of data
+
+    Returns
+    -------
+    s_nm : ndarray, shape (d x 1)
+        combined sample variance of both blocks of data described by input parameters
+    """
+    s_nm = s_m
+
+    if n != 0:
+        s_nm = (((n - 1) * s_n + (m - 1) * s_m) + (n*m*(mu_n - mu_m)**2) / (n + m)) / (n + m - 1) 
+
+    return s_nm
+
+def compare_basis_vectors(U_1, U_2, q):
+    """
+    Quantitatively compare the first q basis vectors of U and U_prime. 
+
+    Parameters
+    ----------
+    U_1 : ndarray, shape (d x a), a >= q
+        first matrix of orthonormal basis vectors
+    U_2 : ndarray, shape (d x b), b >= q
+        second matrix of orthonormal basis vectors
+    q : int
+        number of vectors to compare
+    
+    Returns
+    -------
+    acc : float, 0 <= acc <= 1
+        quantitative measure of distance between basis vectors
+    """
+    if q > min(U_1.shape[1], U_2.shape[1]):
+        print('Desired number of vectors is greater than matrix dimension.')
+        return 0.
+    
+    acc = np.trace(np.abs(U_1[:, :q].T @ U_2[:, :q])) / q
+    return acc
+
+def compression_loss(X, U):
+    """
+    Calculate the compression loss between centered observation matrix X and its rank-q reconstruction.
+
+    Parameters
+    ----------
+    X : ndarray, shape (d x n)
+        flattened, centered image data from n run events
+    U : ndarray, shape (d x q)
+        first q singular vectors of X, forming orthonormal basis of q-dimensional subspace of R^d
+    
+    Returns
+    -------
+    Ln : float
+        compression loss of X
+    """
+    d, n = X.shape
+
+    UX = U.T @ X
+    UUX = U @ UX
+
+    Ln = ((np.linalg.norm(X - UUX, 'fro')) ** 2) / n
+    return Ln 
---
+++ btx/processing/ipca_wrapper.py
@@ -0,0 +1,89 @@
+from btx.interfaces.psana_interface import *
+
+from ipca import *
+
+
+class IPCAWrapper:
+
+    def __init__(self, exp, run, det_type, q=50, block_size=10, num_images=100, init_with_pca=False):
+        self.psi = PsanaInterface(exp=exp, run=run, det_type=det_type)
+
+        self.d = np.prod(self.psi.det.shape())
+        self.reduced_indices = np.array([])
+
+        self.q = q
+        self.m = block_size
+        self.num_images = num_images
+        self.init_with_pca = init_with_pca
+    
+    def initialize_ipca(self):
+        ipca = IPCA(d, q)
+        
+        if self.init_with_pca:
+            ipca.initialize_model()
+
+    def fetch_formatted_images(self, n):
+        d = self.d
+
+        imgs = self.psi.get_images(n)
+
+        formatted_imgs = np.empty((d, n))
+
+        for i in range(n):
+            if self.reduced_indices.size:
+                formatted_imgs[:, i] = np.reshape(imgs[i], (d, 1))[self.reduced_indices]
+            else:
+                formatted_imgs[:, i] = np.reshape(imgs[i], (d, 1))
+
+        return formatted_imgs
+
+    def generate_reduced_indices(self, new_dim):
+        """
+        Sample, without replacement, and store new_dim indices from unassembed image dimension.
+
+        Parameters
+        ----------
+        new_dim : int
+            number of sampled indices to be drawn
+        """
+        det_pixels = np.prod(self.psi.det.shape())
+
+        if det_pixels < new_dim:
+            print('Detector dimension must be greater than or equal to reduced dimension.')
+            return
+
+        self.reduced_indices = np.random.choice(det_pixels, new_dim, replace=False)
+        self.d = new_dim
+
+    def remove_reduced_indices(self):
+        """
+        Remove stored reduced indices.
+        """
+        det_pixels = np.prod(self.psi.det.shape())
+
+        self.reduced_indices = np.array([])
+        self.d = det_pixels
+
+    def run_ipca(self):
+        m = self.m
+        q = self.q
+
+        parsed_images = 0
+
+        if self.init_with_pca:
+            img_block = self.psi.get_images(q, assemble=False)
+            self.ipca.initialize_model(img_block)
+
+            parsed_images = q
+
+        max_images = min(self.num_images, self.psi.max_events)
+
+        while parsed_images <= max_images:
+
+            if parsed_images == max_images or parsed_images % m == 0:
+                current_block_size = parsed_images % m if parsed_images % m else m
+
+                img_block = self.psi.get_images(current_block_size, assemble=False)
+                self.ipca.update_model(img_block)
+            
+            parsed_images += 1
