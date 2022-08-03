d5541ea0277ab3bffce8b45843dcb8b4430a8945
calhep <callumahepworth@gmail.com>
2022-08-03 11:06:04 -0700

    Updated bash script.

--- scripts/benchmark_ipca.sh
+++ scripts/benchmark_ipca.sh
@@ -1,50 +1,28 @@
 #!/bin/bash
 
-usage()
-{
-cat << EOF
-$(basename "$0"):
-  Script to run naive iPCA benchmark.
-
-  OPTIONS:
-    -h|--help
-      Definitions of options
-EOF
-}
-
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-  key="$1"
-  case $key in
-    -h|--help)
-      usage
-      exit
-      ;;
-  esac
-done
-set -- "${POSITIONAL[@]}"
-
 FUNCTION_DIR="/cds/home/h/hepworth/btx-callum/btx/processing/feature_extractor.py"
 COMPS=(1 5 10 20 50 100 150 200)
 
-sbatch << EOF
+for q in ${COMPS[@]}
+do
+    sbatch << EOF
 #!/bin/bash
 
 #SBATCH -p psanaq
-#SBATCH -t 10:00:00
+#SBATCH -t 1:00:00
 #SBATCH --exclusive
-#SBATCH --job-name extractor_benchmark
+#SBATCH --job-name benchmark_${q}
 #SBATCH --ntasks=1
 
 source /reg/g/psdm/etc/psconda.sh -py3  
 
 export PYTHONPATH="${PYTHONPATH}:/cds/home/h/hepworth/btx-callum" 
 
-for q in ${COMPS[@]}; do;
-    mpirun -n 1 python ${FUNCTION_DIR} -e xpptut15 -r 580 -d jungfrau4M --components ${q} --block_size 20 --num_images 200
-    echo "Benchmark with q = ${q} complete."
+mpirun -n -1 python ${FUNCTION_DIR} -e xpptut15 -r 580 -d jungfrau4M --components ${q} --block_size 20 --num_images 200
+echo "Benchmark with q = ${q} complete."
 EOF
+done
+
 echo "Job sent to queue"
 
 
