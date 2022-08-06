92bbd71a2022f70ee9147284674631db6b6d1b1d
calinnet <calinnet31@gmail.com>
2022-08-06 09:42:01 -0700

    Debugging test.

--- scripts/benchmark_ipca.sh
+++ scripts/benchmark_ipca.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 FUNCTION_PATH="/cds/home/h/hepworth/btx-callum/btx/processing/feature_extractor.py"
-COMPS=(1 5 10 25 50 75 100 125 150 165 175 185 195 205 250)
-RANKS=(1 2 4 6 8 10 12 16)
+# COMPS=(1 5 10 25 50 75 100 125 150 165 175 185 195 205 250)
+# RANKS=(1 2 4 6 8 10 12 16)
+
+COMPS=(100)
+RANKS=(1 8)
 
 for r in ${RANKS[@]}
 do
