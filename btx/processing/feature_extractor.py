49f3e957a03cdb3d1c541874b616124049679cf1
calinnet <calinnet31@gmail.com>
2022-08-12 23:47:40 -0700

    Fixed script.

--- btx/processing/feature_extractor.py
+++ btx/processing/feature_extractor.py
@@ -320,10 +320,11 @@ def parse_input():
     parser.add_argument('-r', '--run', help='Run number', required=True, type=int)
     parser.add_argument('-d', '--det_type', help='Detector name, e.g epix10k2M or jungfrau4M',  required=True, type=str)
 
-    parser.add_argument('--output_dir', help='Path to output directory.', required=False, type=str)
     parser.add_argument('-c', '--num_components', help='Number of principal components to compute', required=False, type=int)
     parser.add_argument('-m', '--block_size', help='Desired block size', required=False, type=int)
     parser.add_argument('-n', '--num_images', help='Number of images', required=False, type=int)
+    
+    parser.add_argument('--output_dir', help='Path to output directory.', required=False, type=str)
     parser.add_argument('--init_with_pca', help='Initialize with PCA', required=False, action='store_true')
     parser.add_argument('--benchmark_mode', help='Run algorithm in benchmark mode.', required=False, action='store_true')
     parser.add_argument('--downsample', help='Downsample.', required=False, action='store_true')
