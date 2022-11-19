a83823918eac9e77f18f0d28e5755af17be638f8
Ariana Peck <apeck@pslogin01.slac.stanford.edu>
2022-11-19 12:40:52 -0800

    track more stats as self variables

--- btx/interfaces/istream.py
+++ btx/interfaces/istream.py
@@ -16,6 +16,7 @@ class StreamInterface:
         self.stream_data, self.file_limits_cell, self.file_limits_refn = self.read_all_streams(self.input_files)
         if self.rank == 0:
             self.compute_cell()
+            self.store_stats()
             if not self.cell_only:
                 self.compute_resolution()
     
@@ -198,6 +199,13 @@ class StreamInterface:
         self.cell_params = np.array([np.mean(self.stream_data[key]) for key in keys])
         self.cell_params_std = np.array([np.std(self.stream_data[key]) for key in keys])
         
+    def store_stats(self):
+        """ Store statistics regarding indexing rate as self variables."""
+        counts = np.bincount(self.stream_data['n_lattice'])
+        self.n_indexed = np.sum(counts[1:]) # no. indexed events, including those with multiple lattices
+        self.n_unindexed = counts[0] # no. of events processed from peak finder
+        self.n_multiple = np.sum(counts[2:]) # no. events with multiple lattices
+
     def compute_resolution(self):
         """
         Compute the resolution in inverse Angstrom of each reflection.
@@ -325,14 +333,14 @@ class StreamInterface:
             tag = "_" + tag
         else:
             tag = ""
-        counts = np.bincount(self.stream_data['n_lattice'])
+
         summary_file = os.path.join(os.path.dirname(self.input_files[0]), f"stream{tag}.summary")
         with open(summary_file, 'w') as f:
             f.write("Cell mean: " + " ".join(f"{self.cell_params[i]:.3f}" for i in range(self.cell_params.shape[0])) + "\n")
             f.write("Cell std: " + " ".join(f"{self.cell_params_std[i]:.3f}" for i in range(self.cell_params.shape[0])) + "\n")
-            f.write(f"Number of indexed events: {np.sum(counts[1:])}" + "\n")
-            f.write(f"Fractional indexing rate: {np.sum(counts[1:]) / np.sum(counts):.2f}" + "\n")
-            f.write(f"Fraction of indexed with multiple lattices: {np.sum(counts[2:]) / np.sum(counts[1:]):.2f}" + "\n")
+            f.write(f"Number of indexed events: {self.n_indexed}" + "\n")
+            f.write(f"Fractional indexing rate: {self.n_indexed/(self.n_indexed+self.n_unindexed):.2f}" + "\n")
+            f.write(f"Fraction of indexed with multiple lattices: {self.n_multiple/self.n_indexed:.2f}" + "\n")
             
         # report to elog
         update_url = os.environ.get('JID_UPDATE_COUNTERS')
@@ -526,54 +534,6 @@ def cluster_cell_params(cell, out_clusters, out_cell, in_cell=None, eps=5, min_s
     write_cell_file(results[0][2:], out_cell, input_file=in_cell) 
     
     return clustering.labels_
-
-def plot_cell_trajectory(base_dir, tag, plot=None):
-    """
-    Plot the cell parameter distribution as a function of run number.
-    
-    Parameters
-    ----------
-    base_dir : str
-        directory of indexing results in run folders named r****
-    tag : str
-        sample name or geometry suffix
-    plot : str
-        if given, save plot to png file
-    """
-    fstreams = natural_sort(glob.glob(os.path.join(base_dir, f"r*{tag}.stream")))
-    runs = [int(f.split("/")[-1][1:].split("_")[0]) for f in fstreams]
-    
-    cparams = {}
-    for r,f in zip(runs,fstreams):
-        try:
-            st = StreamInterface([f], cell_only=True)
-            cparams[r] = st.get_cell_parameters()
-        except:
-            print(f"{f} could not be loaded by StreamInterface")
-            
-    cells = np.vstack(np.array([cparams[r] for r in cparams.keys()], dtype='object'))
-    ncryst = np.array([cparams[r].shape[0] for r in cparams.keys()], dtype='object').astype(int)
-    runval = np.repeat(np.array(list(cparams.keys())), ncryst)
-    
-    f, ((ax1,ax2,ax3),(ax4,ax5,ax6)) = plt.subplots(2,3,figsize=(16,8))
-
-    labels = ["$a$", "$b$", "$c$", r"$\alpha$", r"$\beta$", r"$\gamma$"]
-    for i,ax in enumerate([ax1,ax2,ax3,ax4,ax5,ax6]):
-        ax.hexbin(runval, cells[:,i], mincnt=1)
-        if i < 3: 
-            ax.set_title(labels[i], fontsize=14)
-        else: 
-            ax.set_title(labels[i], fontsize=14)
-        ax.set_xlabel("Run no.", fontsize=14)
-        ax.plot([runval.min()-1, runval.max()+1], [np.mean(cells[:,i], axis=0), np.mean(cells[:,i], axis=0)], linestyle='dashed', c='grey')
-        ax.set_xlim(runval.min()-1, runval.max()+1)
-    ax1.set_ylabel("Angstrom", fontsize=14)
-    ax4.set_ylabel("degrees", fontsize=14)
-        
-    f.subplots_adjust(hspace=0.4)
-    
-    if plot is not None:
-        f.savefig(plot, dpi=300, bbox_inches='tight')
 
 #### For command line use ####
             
