0858e01b39913f9c706a5dbc38ed51c8adbcc42d
fredericpoitevin <frederic.poitevin@gmail.com>
2022-11-20 21:06:04 -0800

    creating new DAG to follow run statistics

---
+++ dags/run_stats.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import os
+from airflow import DAG
+from plugins.jid import JIDSlurmOperator
+
+# DAG SETUP
+description='BTX optimize geometry DAG'
+dag_name = os.path.splitext(os.path.basename(__file__))[0]
+
+dag = DAG(
+    dag_name,
+    start_date=datetime( 2022,4,1 ),
+    schedule_interval=None,
+    description=description,
+  )
+
+
+# Tasks SETUP
+
+task_id='run_analysis'
+run_analysis = JIDSlurmOperator( task_id=task_id, dag=dag)
+
+task_id='elog_display'
+elog_display = JIDSlurmOperator(task_id=task_id, dag=dag)
+
+# Draw the DAG
+run_analysis >> elog_display
