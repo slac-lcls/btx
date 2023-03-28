9a6630dca0e31f48101c389ee92d83ce40aad97f
gadorlhiac <gadorlhiac@gmail.com>
2023-03-28 08:48:24 -0700

    Begin SmallData interfacing. Timetool plots.

--- btx/io/ih5.py
+++ btx/io/ih5.py
@@ -1,12 +1,21 @@
 """!
 @brief Utilities for working with HDF5 files and managing concurrent write operations.
 
+Classes:
+SmallDataReader : Interface to HDF5 files written by Small Data.
+
 Functions:
 lock_file(path, timeout) : Decorator to prevent concurrent write operations.
 """
 
-import os, signal, time
+import os
+import numpy as np
+import signal
+import time
 import h5py
+import tables
+import socket
+import matplotlib.pyplot as plt
 
 def lock_file(path:str, timeout: int = 5, wait: float = 0.5) -> callable:
     """! Decorator mainly for write functions. Will not allow the decorated
@@ -23,7 +32,7 @@ def lock_file(path:str, timeout: int = 5, wait: float = 0.5) -> callable:
     my_write_function() # Create shared.lock if it doesn't exist
 
     @param path (str) Path including filename of the file to be locked.
-    @param timeout (int) The timeout time in seconds to abandon execution attempt.
+    @param timeout (int) Timeout time in seconds to abandon execution attempt.
     @param wait (float) Time in seconds to wait between write attempts.
     """
     def decorator_lock(write_func: callable) -> callable:
@@ -59,10 +68,69 @@ def lock_file(path:str, timeout: int = 5, wait: float = 0.5) -> callable:
         return wrapper
     return decorator_lock
 
+
 def _timeout_handler(signum, frame):
     raise TimeoutError()
 
+
 class TimeoutError(Exception):
     """! Error raised if timeout is reached."""
+
     def __init__(self, msg='Timeout reached'):
+        """! Exception initializer."""
         super().__init__(msg)
+
+
+class SmallDataReader:
+    """! Class interface for reading SmallData hdf5 files."""
+
+    def __init__(self, expmt: str,
+                 run: str | int,
+                 savedir: str,
+                 h5path: str | None = None):
+        """! SmallDataReader initializer.
+
+        @param expmt (str) : Experiment name.
+        @param run (str | int) : Run number or range of hyphen-separated runs.
+        @param savedir (str) : Path to write any output files to.
+        @param h5path (str | None) : Path to an hdf5 file. Will search if None.
+        """
+        if h5path:
+            self._path = h5path
+        else:
+            if 'drp' in socket.gethostname():
+                self._path = (f"/cds/data/drpsrcf/{expmt[:3]}/{expmt}/scratch/"
+                              f"hdf5/smalldata/{expmt}_Run{run:04d}")
+            else:
+                self._path = (f"/cds/data/psdm/{expmt[:3]}/{expmt}/"
+                              f"hdf5/smalldata/{expmt}_Run{run:04d}")
+
+        self.expmt = expmt
+        self.run = run
+        self.savedir = savedir
+
+        self.root = tables.File(self._path).root
+        self.h5 = h5py.File(self._path)
+
+    def timetool_histograms(self):
+        """! Pass."""
+        fltpos_ps = self.h5['tt/FLTPOS_PS'][:]
+        # fltpos = self.h5['tt/FLTPOS'][:]
+        ampl = self.h5['tt/AMPL'][:]
+        fwhm = self.h5['tt/FLTPOSFWHM'][:]
+
+        fig, axs = plt.subplots(1, 3, figsize=(6, 4), dpi=200)
+        # axs[0].hist(fltpos, bins=30)
+        axs[0].hist(fltpos_ps, bins=30, density=True)
+        axs[0].set_title('Timetool Correction')
+        axs[0].set_xlabel('Correction (ps)')
+        axs[0].set_ylabel('Density')
+        # axs[0].hist(fltpos)
+        axs[1].hist(ampl, bins=30, density=True)
+        axs[1].set_title('Convolution\nAmplitude')
+        axs[1].set_xlabel('Amplitude (a.u.)')
+        axs[2].hist(fwhm, bins=30, density=True)
+        axs[2].set_title('Convolution FWHM')
+        axs[2].set_xlabel('FWHM (a.u.)')
+        fig.tight_layout()
+        fig.savefig(f'{self.savedir}/tt_histograms_r{self.run:04d}.png')
