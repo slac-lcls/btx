c1475c440d7de62c65cbce853824306454bffa87
gadorlhiac <gadorlhiac@gmail.com>
2023-03-28 16:01:42 -0700

    Typing for < Python 3.10. Image sum plotting.

--- btx/io/ih5.py
+++ btx/io/ih5.py
@@ -16,6 +16,7 @@ import h5py
 import tables
 import socket
 import matplotlib.pyplot as plt
+from typing import Union, Optional
 
 def lock_file(path:str, timeout: int = 5, wait: float = 0.5) -> callable:
     """! Decorator mainly for write functions. Will not allow the decorated
@@ -85,9 +86,9 @@ class SmallDataReader:
     """! Class interface for reading SmallData hdf5 files."""
 
     def __init__(self, expmt: str,
-                 run: str | int,
+                 run: Union[str, int],
                  savedir: str,
-                 h5path: str | None = None):
+                 h5path: Optional[str] = None):
         """! SmallDataReader initializer.
 
         @param expmt (str) : Experiment name.
@@ -112,20 +113,48 @@ class SmallDataReader:
         self.root = tables.File(self._path).root
         self.h5 = h5py.File(self._path)
 
+    def image_sums(self):
+        """! Plots the sums of the images over a run for the used detectors.
+
+        The image sums (powder patterns for crystallography) can be used to
+        verify that data was collected during a run.
+        """
+        imgList = [self.h5[f'Sums/{img}'] for img in self.h5['Sums'].keys()]
+
+        for img in imgList:
+            squeezed = img[:].squeeze()
+            if len(squeezed.shape) > 2:
+                self.construct_img_panels(img)
+            else:
+                lims = (np.nanpercentile(squeezed, 5),
+                        np.nanpercentile(squeezed, 95))
+                title = img.name.split('/')[2]
+                fig, ax = plt.subplots(1, 1, figsize=(4, 4), dpi=200)
+                ax.imshow(squeezed, clim=lims)
+                ax.set_title(title)
+                fig.tight_layout()
+                fig.savefig(f'{self.savedir}/{title}_Sum_r{self.run:04d}')
+
+    def construct_img_panels(self):
+        """! Assemble multipanel detector images."""
+        pass
+
     def timetool_histograms(self):
-        """! Pass."""
+        """! Plot histograms of timetool output.
+
+        Plots the histograms of: the timetool correction, the convolution
+        amplitudes, and the full-width half-maximums (FWHMs) of the
+        convolutions.
+        """
         fltpos_ps = self.h5['tt/FLTPOS_PS'][:]
-        # fltpos = self.h5['tt/FLTPOS'][:]
         ampl = self.h5['tt/AMPL'][:]
         fwhm = self.h5['tt/FLTPOSFWHM'][:]
 
         fig, axs = plt.subplots(1, 3, figsize=(6, 4), dpi=200)
-        # axs[0].hist(fltpos, bins=30)
         axs[0].hist(fltpos_ps, bins=30, density=True)
         axs[0].set_title('Timetool Correction')
         axs[0].set_xlabel('Correction (ps)')
         axs[0].set_ylabel('Density')
-        # axs[0].hist(fltpos)
         axs[1].hist(ampl, bins=30, density=True)
         axs[1].set_title('Convolution\nAmplitude')
         axs[1].set_xlabel('Amplitude (a.u.)')
@@ -134,3 +163,34 @@ class SmallDataReader:
         axs[2].set_xlabel('FWHM (a.u.)')
         fig.tight_layout()
         fig.savefig(f'{self.savedir}/tt_histograms_r{self.run:04d}.png')
+
+    def print_all_nodes(self):
+        """! Print all groups and datasets in the HDF5 file."""
+        self.traverse_nodes(self.h5, level=1)
+
+    def traverse_nodes(self, node, level: int = 1):
+        """! Recurses through an HDF5 file hierarchy printing nodes."""
+        if type(node) == h5py._hl.dataset.Dataset:
+            return
+        else:
+            for key in node.keys():
+                print('\t'*(level - 1) + f'{key}\n')
+                self.traverse_nodes(node[key], level=level + 1)
+
+    def write_stamped_values(self, node_list: list):
+        """! Write a text file of unique event identifiers and h5 node values.
+
+        This can be used to write out e.g. specific evr codes per event. Data
+        is written in a space separated format, with one event per line. The
+        first column contains event identifiers, followed by the values for
+        each node in subsequent columns. E.g.:
+        unique-identifier node1        node2 ...
+                id_event1 event1-node1 event1-node2
+                id_event2 event2-node1 event2-node2
+                   ...
+
+        Unique identifiers are of the form: `event_time-fiducial`
+
+        @param node_list (list[str])
+        """
+        pass
