e2a5b434eea71459c4cc60f208f7e895aac47879
fredericpoitevin <frederic.poitevin@gmail.com>
2023-06-20 12:33:27 -0700

    temporary debugging action

--- btx/processing/indexer.py
+++ btx/processing/indexer.py
@@ -98,7 +98,8 @@ class Indexer:
         js = JobScheduler(self.tmp_exe, ncores=self.ncores, jobname=f'idx_r{self.run:04}', queue=self.queue, time=self.time)
         js.write_header()
         js.write_main(command, dependencies=['crystfel'] + self.methods.split(','))
-        js.clean_up()
+        # debugging
+        #js.clean_up()
         js.submit()
         logger.info(f"Indexing executable submitted: {self.tmp_exe}")
 
