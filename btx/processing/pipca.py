d0bbccabe4cd0851f3b89ed665e844529b58a373
Russell Marasigan <marasign@sdflogin001.sdf.slac.stanford.edu>
2023-07-27 20:11:12 -0700

    added display_dashboard function to pipca

--- btx/processing/pipca.py
+++ btx/processing/pipca.py
@@ -6,6 +6,13 @@ from mpi4py import MPI
 from matplotlib import pyplot as plt
 from matplotlib import colors
 
+import holoviews as hv
+hv.extension('bokeh')
+from holoviews.streams import Params
+
+import panel as pn
+import panel.widgets as pnw
+
 from btx.misc.shortcuts import TaskTimer
 
 from btx.interfaces.ipsana import (
@@ -41,6 +48,7 @@ class PiPCA:
 
         self.psi = PsanaInterface(exp=exp, run=run, det_type=det_type)
         self.psi.counter = start_offset
+        self.start_offset = start_offset
 
         self.priming = priming
         self.downsample = downsample
@@ -161,6 +169,8 @@ class PiPCA:
         # update model with remaining batches
         for batch_size in batch_sizes:
             self.fetch_and_update_model(batch_size)
+            
+        print("Model complete")
 
     def get_formatted_images(self, n, start_index, end_index):
         """
@@ -278,7 +288,7 @@ class PiPCA:
 
             with TaskTimer(self.task_durations, "record pc data"):
                 if n > 0:
-                    self.record_loadings(X, 5)
+                    self.record_loadings(X, q)
 
             with TaskTimer(self.task_durations, "update mean and variance"):
                 mu_n = self.mu
@@ -672,6 +682,93 @@ class PiPCA:
 
         plt.show()
 
+        
+    def display_dashboard(self):
+        """
+        Displays a pipca dashboard with a PC plot and intensity heatmap.
+        """
+        
+        start_img = self.start_offset
+        
+        # Create PC dictionary and widgets
+        PCs = {f'PC{i}' : v for i, v in enumerate(self.pc_data, start=1)}
+        PC_options = list(PCs)
+        
+        PCx = pnw.Select(name='X-Axis', value='PC1', options=PC_options)
+        PCy = pnw.Select(name='Y-Axis', value='PC2', options=PC_options)
+        widgets_scatter = pn.WidgetBox(PCx, PCy, width=100)
+        
+        tap_source = None
+        posxy = hv.streams.Tap(source=tap_source, x=0, y=0)
+        
+        # Create PC scatter plot
+        @pn.depends(PCx.param.value, PCy.param.value)
+        def create_scatter(PCx, PCy):
+            img_index_arr = np.arange(start_img, start_img + len(PCs[PCx]))
+            scatter_data = {**PCs, 'Image': img_index_arr}
+            
+            opts = dict(width=400, height=300, color='Image', cmap='rainbow', 
+                        colorbar=True, show_grid=True, toolbar='above', tools=['hover'])
+            scatter = hv.Points(scatter_data, kdims=[PCx, PCy], vdims=['Image'], 
+                                label="%s vs %s" % (PCx.title(), PCy.title())).opts(**opts)
+            
+            posxy.source = scatter
+            return scatter
+        
+        # Define function to compute heatmap based on tap location
+        def tap_heatmap(x, y, pcx, pcy):
+            # Finds the index of image closest to the tap location
+            img_source = None
+            min_diff = None
+            square_diff = None
+            
+            for i, (xv, yv) in enumerate(zip(PCs[pcx], PCs[pcy])):    
+                square_diff = (x - xv) ** 2 + (y - yv) ** 2
+                if (min_diff is None or square_diff < min_diff):
+                    min_diff = square_diff
+                    img_source = i
+            
+            # Downsample so heatmap is at most 100 x 100
+            counter = self.psi.counter
+            self.psi.counter = start_img + img_source
+            img = self.psi.get_images(1)
+            _, x_pixels, y_pixels = img.shape
+            self.psi.counter = counter
+            
+            max_pixels = 100
+            bin_factor_x = int(x_pixels / max_pixels)
+            bin_factor_y = int(y_pixels / max_pixels)
+            
+            while x_pixels % bin_factor_x != 0:
+                bin_factor_x += 1
+            while y_pixels % bin_factor_y != 0:
+                bin_factor_y += 1
+            
+            img = img.reshape((x_pixels, y_pixels))
+            binned_img = img.reshape(int(x_pixels / bin_factor_x),
+                                        bin_factor_x,
+                                        int(y_pixels / bin_factor_y),
+                                        bin_factor_y).mean(-1).mean(1)
+            
+            # Creates hm_data array for heatmap
+            bin_x_pixels, bin_y_pixels = binned_img.shape
+            rows = np.tile(np.arange(bin_x_pixels).reshape((bin_x_pixels, 1)), bin_y_pixels).flatten()
+            cols = np.tile(np.arange(bin_y_pixels), bin_x_pixels)
+            
+            hm_data = np.stack((rows, cols, binned_img.flatten()))
+            hm_data = hm_data.T.reshape((bin_x_pixels * bin_y_pixels, 3))
+        
+            opts = dict(width=400, height=300, cmap='plasma', colorbar=True, toolbar='above')
+            heatmap = hv.HeatMap(hm_data, label="Image %s" % (start_img+img_source)).aggregate(function=np.mean).opts(**opts)
+            
+            return heatmap
+            
+        # Connect the Tap stream to the tap_heatmap callback
+        stream1 = [posxy]
+        stream2 = Params.from_params({'pcx': PCx.param.value, 'pcy': PCy.param.value})
+        tap_dmap = hv.DynamicMap(tap_heatmap, streams=stream1+stream2)
+        
+        return pn.Row(widgets_scatter, create_scatter, tap_dmap).servable('Cross-selector')
 
 def distribute_indices_over_ranks(d, size):
     """
