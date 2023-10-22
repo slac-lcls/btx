d66461d9682db32c3e863b7df7ed178deeeda0ad
gadorlhiac <gadorlhiac@gmail.com>
2023-10-22 14:19:27 -0700

    BUG: Add space for autogenerated workflow between queue and reservation flags

--- scripts/setup_btx.py
+++ scripts/setup_btx.py
@@ -51,13 +51,13 @@ def determine_configuration(
     exp_dir: str = f"/sdf/data/lcls/ds/{exp[:3]}/{exp}"
     config_file: str = f"{exp_dir}/scratch/btx/yamls/config.yaml"
     param_string: str = (
-        f"-a {args.account} -c {config_file} -d {dag} -n {args.ncores}"
+        f"-a {args.account} -c {config_file} -d {dag} -n {args.ncores} "
         f"-q {args.queue}"
     )
 
     reservation: str = args.reservation
     if reservation:
-        param_string += f"-r {reservation}"
+        param_string += f" -r {reservation}"
 
     if args.verbose:
         param_string += f"--verbose"
